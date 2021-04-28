41932f4b73199d38b10021ad94619e12596a5087
Leandro Montesdeoca <leamontes97@gmail.com>
2021-04-28 18:49:41 -0300

    Primer push de obligatorio

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dominio;
+namespace Obligatorio1
+{
+    class Program
+    {
+        private static Sistema unSistema = new Sistema;
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
